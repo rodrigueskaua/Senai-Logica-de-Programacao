2607d6fefe280b81cd419839f72fa68df6a2cd13
KauaF-BR <Nascimento200408@gmail.com>
2021-10-07 22:30:35 -0300

    Create Exemplo_repetição_while.py

---
+++ Estrutura de Repetição/Exemplo_repetição_while.py
@@ -0,0 +1,24 @@
+
+#Exemplo de estrutura de repetição while
+"""
+letra = "?"
+qtd = 0
+while not letra == "S":
+    qtd += 1
+    letra = input("Digite S para sair: \n")
+    print(f"Você já tentou {qtd} vezes")
+print("Fim do programa!")
+"""
+
+"""
+salario = float(input("Qual é o seu salário atual? R$ "))
+while salario < 15000:
+    salario += 1100
+    print(f"Aumentado o salário! valor atual: R${salario}")
+print(f"Meta atingida salário valor atual: R${salario} ")
+"""
+
+    curso = input("Qual é o curso mais top?")
+
+while curso != "DS":
+print("Acertou, parabéns! :D")
